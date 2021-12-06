cf57331373433630e99c8dca785f18ac09b31bbf
Emilia Andersson <emilia.andersson@helsinki.fi>
2021-12-06 19:34:45 +0200

    Viikko 5, 5

--- viikko5/laskin/src/komennot.py
+++ viikko5/laskin/src/komennot.py
@@ -1,33 +1,32 @@
-class Summa:
+class Komento:
     def __init__(self, sovelluslogiikka, lue_syote):
         self._sovelluslogiikka = sovelluslogiikka
         self.lue_syote = lue_syote
+
+class Summa(Komento):
+    def __init__(self, sovelluslogiikka, lue_syote):
+        super().__init__(sovelluslogiikka, lue_syote)
 
     def suorita(self):
         self._sovelluslogiikka.plus(int(self.lue_syote()))
 
-class Erotus:
+class Erotus(Komento):
     def __init__(self, sovelluslogiikka, lue_syote):
-        self._sovelluslogiikka = sovelluslogiikka
-        self.lue_syote = lue_syote
+        super().__init__(sovelluslogiikka, lue_syote)
 
     def suorita(self):
         self._sovelluslogiikka.miinus(int(self.lue_syote()))
 
-class Nollaus:
+class Nollaus(Komento):
     def __init__(self, sovelluslogiikka, lue_syote):
-        self._sovelluslogiikka = sovelluslogiikka
-        self.lue_syote = lue_syote
+        super().__init__(sovelluslogiikka, lue_syote)
 
     def suorita(self):
         self._sovelluslogiikka.nollaa()
 
-class Kumoa:
+class Kumoa(Komento):
     def __init__(self, sovelluslogiikka, lue_syote):
-        self._sovelluslogiikka = sovelluslogiikka
-        self.lue_syote = lue_syote
+        super().__init__(sovelluslogiikka, lue_syote)
 
     def suorita(self):
-        pass
-
-
+        self._sovelluslogiikka.kumoa()
